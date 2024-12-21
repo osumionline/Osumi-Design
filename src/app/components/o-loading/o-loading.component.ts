4090f8437994f844fdbc09e511741c405e8f6454
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2024-12-21 18:30:54 +0100

    Avanzo con diseño y funcionalidad de OButton

--- src/app/components/o-loading/o-loading.component.ts
+++ src/app/components/o-loading/o-loading.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
     height="24"
     viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg"
+    style="vertical-align: bottom;"
   >
     <style>
       .spinner_ajPY {
