70134f301e9b136e6a33a625fd5ef428841c9c82
Iñigo Gorosabel <inigo.gorosabel@idom.com>
2025-01-10 13:59:40 +0100

    Avanzo con pagina de OButton

--- src/app/pages/components/o-button-docs/o-button-docs.component.ts
+++ src/app/pages/components/o-button-docs/o-button-docs.component.ts
@@ -1,9 +1,11 @@
 import { Component, signal, WritableSignal } from '@angular/core';
 import { OButtonComponent } from '../../../components/o-button/o-button.component';
+import { OTabComponent } from '../../../components/o-tab/o-tab.component';
+import { OTabsComponent } from '../../../components/o-tabs/o-tabs.component';
 
 @Component({
   selector: 'o-button-docs',
-  imports: [OButtonComponent],
+  imports: [OButtonComponent, OTabsComponent, OTabComponent],
   templateUrl: './o-button-docs.component.html',
   styleUrl: './o-button-docs.component.scss',
 })
